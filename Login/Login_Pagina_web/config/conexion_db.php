2aa661a2a5d582e0469634ef06c6e1f4beda9d83
Aldopuello1999 <72235838+Aldopuello1999@users.noreply.github.com>
2023-01-20 09:08:00 -0500

    se agrego el nombre de la base de datos y cambio en el mensaje al conectar la base de datos

--- Login/Login_Pagina_web/config/conexion_db.php
+++ Login/Login_Pagina_web/config/conexion_db.php
@@ -1,15 +1,15 @@
 <?php 
     // ------------- Conexión a la Base de Datos de phpmyadmin --------------------
     $servername = "localhost"; // ---> localhost o la IP
-    $database = "database"; // ---> nombre de la Base de Datos
-    $username = "username"; // ---> usuario para ingresar 
-    $password = "password"; // ---> Contraseña para ingresar 
+    $database = "login"; // ---> nombre de la Base de Datos
+    $username = "root"; // ---> usuario para ingresar 
+    $password = ""; // ---> Contraseña para ingresar 
 
     $enlance = mysqli_connect($servername,$username,$password,$database);
 
     if(!$enlance){
-        die("Conexion exitosa: " . mysqli_connect_error());
+        die("Conexion fallida: " . mysqli_connect_error());
     }
-    echo "Conexion fallida";
+    echo "Conexion exitosa";
     mysqli_close($enlance);
 ?>
