a09df6b3dfd57589c7254834a9eb818b5ab8bf0a
Aldopuello1999 <72235838+Aldopuello1999@users.noreply.github.com>
2023-01-20 09:57:34 -0500

    se agrego el encabecado de la pagina en el footer, head, header.

--- Login/Login_Pagina_web/index.php
+++ Login/Login_Pagina_web/index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Web</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    require_once("c://xampp/htdocs/Login/Login/Login_Pagina_web/view/head/header.php");
+?>
+
+
+<?php
+    require_once("c://xampp/htdocs/Login/Login/Login_Pagina_web/view/head/footer.php");
+?>
